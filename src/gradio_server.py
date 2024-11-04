d02a4014f1b51d86366d89d38c1a18325b58f787
shengj8329 <shengjie8329@163.com>
2024-11-04 09:47:18 +0800

    add v0.7 homework branch

--- src/gradio_server.py
+++ src/gradio_server.py
@@ -10,6 +10,7 @@ from content_assistant import ContentAssistant
 from image_advisor import ImageAdvisor
 from input_parser import parse_input_text
 from ppt_generator import generate_presentation
+from src.minicpm_v_model import chat_with_image
 from template_manager import load_template, get_layout_mapping
 from layout_manager import LayoutManager
 from logger import LOG
@@ -56,12 +57,12 @@ def generate_contents(message, history):
                 audio_text = asr(uploaded_file)
                 texts.append(audio_text)
             # 解释说明图像文件
-            # elif file_ext in ('.jpg', '.png', '.jpeg'):
-            #     if text_input:
-            #         image_desc = chat_with_image(uploaded_file, text_input)
-            #     else:
-            #         image_desc = chat_with_image(uploaded_file)
-            #     return image_desc
+            elif file_ext in ('.jpg', '.png', '.jpeg'):
+                if text_input:
+                    image_desc = chat_with_image(uploaded_file, text_input)
+                else:
+                    image_desc = chat_with_image(uploaded_file)
+                return image_desc
             # 使用 Docx 文件作为素材创建 PowerPoint
             elif file_ext in ('.docx', '.doc'):
                 # 调用 generate_markdown_from_docx 函数，获取 markdown 内容
